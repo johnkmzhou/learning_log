2510d7675702c7723d447ec7bd428baf2d7a694b
John Zhou <john.km.zhou@gmail.com>
2017-09-09 01:14:55 -1000

    Add new topic form. Add requirements.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	url(r'^topics/$', views.topics, name = 'topics'),
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+	url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
 	"""The home page for Learning Log"""
@@ -17,3 +20,17 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+	"""Add a new topic."""
+	if request.method != 'POST':
+		# No data submitted; create a blank form.
+		form = TopicForm()
+	else:
+		# POST data submitted; process data.
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+	context = {'form' : form}
+	return render(request, 'learning_logs/new_topic.html', context)
