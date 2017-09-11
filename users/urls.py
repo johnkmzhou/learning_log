52d12a318334638a58119802545f2b7f5d4e4bc2
John Zhou <john.km.zhou@gmail.com>
2017-09-10 22:01:49 -1000

    Add user registration.

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
-    url(r'^logout/$', views.logout_view, name='logout')   
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register')
 ]
